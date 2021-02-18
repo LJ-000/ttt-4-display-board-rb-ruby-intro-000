8941f5972fc2f706748bfee05ae6306c5f3b96c5
friendly-designer-db82 <nielsen.j.laura@gmail.com>
2021-02-18 18:28:07 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -10,101 +10,32 @@ board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 display_board (board)
 
 
-  board = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
+board = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
+display_board (board)
 
-  puts " #{board[0]} | #{board[1]} | #{board[2]} "
-  puts "-----------"
-  puts " #{board[3]} | #{board[4]} | #{board[5]} "
-  puts "-----------"
-  puts " #{board[6]} | #{board[7]} | #{board[8]} "
-end
+board = ["O", " ", " ", " ", " ", " ", " ", " ", " "]
+display_board (board)
 
-def display_board(board)
-  board = ["O", " ", " ", " ", " ", " ", " ", " ", " "]
+board = ["O", " ", " ", " ", "X", " ", " ", " ", " "]
+display_board (board)
 
-  puts " #{board[0]} | #{board[1]} | #{board[2]} "
-  puts "-----------"
-  puts " #{board[3]} | #{board[4]} | #{board[5]} "
-  puts "-----------"
-  puts " #{board[6]} | #{board[7]} | #{board[8]} "
-end
+board = ["X", "X", "X", " ", " ", " ", " ", " ", " "]
+display_board (board)
 
-def display_board(board)
-  board = ["O", " ", " ", " ", "X", " ", " ", " ", " "]
+board = [" ", " ", " ", " ", " ", " ", "O", "O", "O"]
+display_board (board)
 
-  puts " #{board[0]} | #{board[1]} | #{board[2]} "
-  puts "-----------"
-  puts " #{board[3]} | #{board[4]} | #{board[5]} "
-  puts "-----------"
-  puts " #{board[6]} | #{board[7]} | #{board[8]} "
-end
+board = ["X", " ", " ", " ", "X", " ", " ", " ", "X"]
+display_board (board)
 
-def display_board(board)
-  board = ["X", "X", "X", " ", " ", " ", " ", " ", " "]
+board = [" ", " ", "O", " ", "O", " ", "O", " ", " "]
+display_board (board)
 
-  puts " #{board[0]} | #{board[1]} | #{board[2]} "
-  puts "-----------"
-  puts " #{board[3]} | #{board[4]} | #{board[5]} "
-  puts "-----------"
-  puts " #{board[6]} | #{board[7]} | #{board[8]} "
-end
+board = ["X", " ", "O", " ", "O", " ", "O", " ", " "]
+display_board (board)
 
-def display_board(board)
-  board = [" ", " ", " ", " ", " ", " ", "O", "O", "O"]
+board = ["X", "X", "X", "X", "X", "X", "X", "X", "X"]
+display_board (board)
 
-  puts " #{board[0]} | #{board[1]} | #{board[2]} "
-  puts "-----------"
-  puts " #{board[3]} | #{board[4]} | #{board[5]} "
-  puts "-----------"
-  puts " #{board[6]} | #{board[7]} | #{board[8]} "
-end
-
-def display_board(board)
-  board = ["X", " ", " ", " ", "X", " ", " ", " ", "X"]
-
-  puts " #{board[0]} | #{board[1]} | #{board[2]} "
-  puts "-----------"
-  puts " #{board[3]} | #{board[4]} | #{board[5]} "
-  puts "-----------"
-  puts " #{board[6]} | #{board[7]} | #{board[8]} "
-end
-
-def display_board(board)
-  board = [" ", " ", "O", " ", "O", " ", "O", " ", " "]
-
-  puts " #{board[0]} | #{board[1]} | #{board[2]} "
-  puts "-----------"
-  puts " #{board[3]} | #{board[4]} | #{board[5]} "
-  puts "-----------"
-  puts " #{board[6]} | #{board[7]} | #{board[8]} "
-end
-
-def display_board(board)
-  board = ["X", " ", "O", " ", "O", " ", "O", " ", " "]
-
-  puts " #{board[0]} | #{board[1]} | #{board[2]} "
-  puts "-----------"
-  puts " #{board[3]} | #{board[4]} | #{board[5]} "
-  puts "-----------"
-  puts " #{board[6]} | #{board[7]} | #{board[8]} "
-end
-
-def display_board(board)
-  board = ["X", "X", "X", "X", "X", "X", "X", "X", "X"]
-
-  puts " #{board[0]} | #{board[1]} | #{board[2]} "
-  puts "-----------"
-  puts " #{board[3]} | #{board[4]} | #{board[5]} "
-  puts "-----------"
-  puts " #{board[6]} | #{board[7]} | #{board[8]} "
-end
-
-def display_board(board)
-  board = ["O", "O", "O", "O", "O", "O", "O", "O", "O"]
-
-  puts " #{board[0]} | #{board[1]} | #{board[2]} "
-  puts "-----------"
-  puts " #{board[3]} | #{board[4]} | #{board[5]} "
-  puts "-----------"
-  puts " #{board[6]} | #{board[7]} | #{board[8]} "
-end
+board = ["O", "O", "O", "O", "O", "O", "O", "O", "O"]
+display_board (board)
