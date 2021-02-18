34f08ca9724580d9f6ad7e3bf50ca2a08bd0e651
friendly-designer-db82 <nielsen.j.laura@gmail.com>
2021-02-18 18:19:53 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -6,6 +6,8 @@ def display_board (board)
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
+
+  display_board (board)
 end
 
 def display_board(board)
