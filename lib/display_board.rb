8bca7f73de829c6e4cafd78f2f6cfef211479988
friendly-designer-db82 <nielsen.j.laura@gmail.com>
2021-02-18 18:21:33 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -89,3 +89,23 @@ def display_board(board)
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
+
+def display_board(board)
+  board = ["X", "X", "X", "X", "X", "X", "X", "X", "X"]
+
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def display_board(board)
+  board = ["O", "O", "O", "O", "O", "O", "O", "O", "O"]
+
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
